2bb998581a9bddfb61af87390a771679ae28b94d
thomas hryckewycz <thryckz@Live.com>
2020-01-02 14:18:18 -0500

    Added project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paladin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
